5dc033593825307cec114725b0cdf24af190ed22
joanni46 <evgenijserdukov58@gmail.com>
2024-04-18 19:28:02 +0300

    start_project

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+
+
+
+
+
+
+
+       
+        
+                char my_name[] = new char[4];
+                my_name[0] = 'Ж';
+                my_name[1] = 'Е';
+                my_name[2] = 'Н';
+                my_name[3] = 'Я';
+        
+                System.out.println(my_name);
+        
+    
+
+
+
+
+
+
+    }
+}
